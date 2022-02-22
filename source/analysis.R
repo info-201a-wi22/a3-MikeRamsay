281b32d786d5d50bda69eb7effeb1eee1317add2
MikeRamsay <mramsay2@uw.edu>
2022-02-22 14:51:10 -0800

    Commit 1

--- source/analysis.R
+++ source/analysis.R
@@ -1,1 +1,15 @@
 #Code goes here
+library("dplyr")
+library("ggplot2")
+library("plotly")
+library("readr")
+
+urlfile="https://raw.githubusercontent.com/vera-institute/incarceration-trends/ae6a18945c23b56aa8d4b00ff83090d83a171fac/incarceration_trends.csv"
+
+incarceration_dataset <- read_csv(url(urlfile))
+spec(incarceration_dataset)
+View(incarceration_dataset)
+dim(incarceration_dataset)
+
+
+
