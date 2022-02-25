f2b5aa64698837bf1212ba7f96eb787001d01674
MikeRamsay <mramsay2@uw.edu>
2022-02-25 00:13:59 -0800

    Commit 4

--- source/analysis.R
+++ source/analysis.R
@@ -3,6 +3,7 @@ library("ggplot2")
 library("plotly")
 library("readr")
 library("leaflet")
+library("knitr")
 
 #Loading dataset into R:
 urlfile="https://raw.githubusercontent.com/vera-institute/incarceration-trends/ae6a18945c23b56aa8d4b00ff83090d83a171fac/incarceration_trends.csv"
@@ -11,10 +12,11 @@ incarceration_dataset <- read_csv(url(urlfile))
 spec(incarceration_dataset)
 View(incarceration_dataset)
 dim(incarceration_dataset)
+clean_incarceration_dataset <- na.omit(incarceration_dataset)
 
 #-------------------------------------------------------------------------------
 #My 5 values of interest:
-
+#summary_info <- list()
 #Value #1: What is the average jail population across all counties in 2018?
 av_jail_pop <- incarceration_dataset %>%
   filter(year == 2018) %>%
@@ -50,43 +52,88 @@ jail_pop_1980_to_2000 <- jail_pop_2000 - jail_pop_1980
 
 #Value #5: What is the average proportion of blacks in jails by state?
 prop_blacks <- clean_incarceration_dataset %>%
-  summarize(prop_blacks = sum(black_jail_pop, na.rm = TRUE)/sum(total_jail_pop, na.rm = TRUE)) %>%
+  summarize(prop_blacks = sum(black_jail_pop, na.rm = TRUE)/sum(total_jail_pop, na.rm = TRUE)*100) %>%
   pull(prop_blacks)
 
 #-------------------------------------------------------------------------------
 #Trends Over Time Chart:
-#(Use Q 3- yr vs pop) 3: How has total jail population changed from 1970 to 2018, by race?
-# Or: Better: Total jail population in CA, by race, from 1970 to 2018
+#Total jail population in CA from 2000 to 2013
+time_plot_data <- clean_incarceration_dataset %>%
+  filter(state == "CA") %>%
+  group_by(year) %>%
+  summarise(combo_total_jail_pop = sum(total_jail_pop)) 
 
+time_plot <- ggplot(data = time_plot_data) +
+  geom_line(mapping = aes(x = year, y = combo_total_jail_pop)) +
+  labs(
+    title = "Total jail population in California from 2000 to 2013",
+    x = "Year (2000-2013)",
+    y = "Total Jail Population"
+  )
+ggplotly(time_plot)
 
+View(time_plot_data)
 #-------------------------------------------------------------------------------
 #Variable Comparison Chart:
-#(Use Q 2) or compare latinx pop vs total in jail from ice
-#2: Does commuting zone have an affect on jail population?
+#Comparing latinx population vs total in jail from ice
+var_comparison_data <- clean_incarceration_dataset %>%
+  filter(latinx_pop_15to64 <= 1000000)
+  
+var_comparison_plot <- ggplot(data = var_comparison_data) +
+  geom_point(mapping = aes(x = latinx_pop_15to64, y = total_jail_from_ice)) +
+  geom_smooth(mapping = aes(x = latinx_pop_15to64, y = total_jail_from_ice)) +
+  labs(
+    title = "Comparing Latinx Population vs Total in Jail from ICE",
+    x = "Latinx Population",
+    y = "Population in Jail from ICE"
+  ) 
 
+ggplotly(var_comparison_plot)
 
+View(var_comparison_data)
 #-------------------------------------------------------------------------------
 #Map: 
-#(Use Q 1 or 5) 5: Which state has the largest proportion of (prev answer) people in jails?
-#1: What state had the highest portion of its residents in jails in 2018?
-  #Use total_pop_15to64 for this - incorporate race?
+#What state had the highest (portion) amount of its residents in jails in 2000?
 
 new_incarceration <- incarceration_dataset %>%
-  state.abb[match(state,state.name)]
+  state.abb[match(state, state.name)]
 View(new_incarceration)
 
-highest_jail_pop <- incarceration_dataset %>%
-  group_by(state) %>%
-  filter(year == 2018) %>%
-  mutate(prop_in_jail = (total_jail_pop/total_pop_15to64)*100) %>%
-  select(state, county_name, prop_in_jail) %>%
+highest_jail_pop <- clean_incarceration_dataset %>%
+  filter(year == 2000) %>%
+  mutate(state = tolower(state))
+  
+state_shape <- map_data("state") %>%
+  rename(state = region) %>%
+  left_join(incarceration_dataset, by="state")
+
+ggplot(state_shape) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = total_jail_pop),
+    color = "white",
+    size = .1
+  ) + 
+  coord_map()+
+  scale_fill_continuous(low = "#132B43", high = "Red") +
+  labs(fill = "Jail Populations") 
+
+View(state_shape)
+
+
+  #mutate(prop_in_jail = sum(total_jail_pop)/sum(total_pop)*100) %>%
+  #group_by(state) %>%
+  #summarise(combo_total_jail_pop = sum(total_jail_pop)) %>%
+  #summarise(combo_total_pop = sum(total_pop)) %>%
+  #summarise(combo_prop_in_jail = sum(prop_in_jail))
+  
+  select(state, prop_in_jail) #%>%
   state.abb[match("state",state.name)]
 
 View(highest_jail_pop)
 
 
-states <- map_data("state")
-ggplot(states) +
+state_shape <- map_data("state")
+ggplot(state_shape) +
   geom_polygon(
     mapping = aes(x = long, y = lat, group = group),
     color = "white",
